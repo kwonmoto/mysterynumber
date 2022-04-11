0002f851736b16d32b8b51b8a613e7d2ac00e2bf
kwonmoto <spadekwon@gmail.com>
2022-04-11 17:36:28 +0900

    initial

---
+++ src/container/Question.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Question() {
+  return (
+    <div className="container">
+      <div className="number">
+        <h1>2</h1>
+      </div>
+      <div className="mark">
+        <h1>?</h1>
+      </div>
+      <div className="number">
+        <h1>3</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Question;
---
+++ src/container/Title.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title() {
+  return (
+    <h1>
+      <code>Mystery Number</code>
+    </h1>
+  );
+}
+
+export default Title;
